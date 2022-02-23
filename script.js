d34df0c1da7c8a43a09af1d198668d244330d341
Victoria <victoriyasotscaya2050@gmail.com>
2022-02-24 00:49:17 +0200

    Added Task2

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ const birthMounth = birthdayDate.getMonth() + 1;
 const birthDay = birthdayDate.getDate();
 
 console.log(`${birthDay}.0${birthMounth}.${birthYear}`);
+
+//Task2
+function getDiffDays(start, end) {
+  const dateStart = new Date(start);
+  const dateEnd = new Date(end);
+  const oneDayMs = 86400000;
+
+  const passedDays = (dateEnd.getTime() - dateStart.getTime()) / oneDayMs;
+
+  if (
+    dateStart.toString() === "Invalid Date" ||
+    dateEnd.toString() === "Invalid Date"
+  ) {
+    console.error("Error: invalid date");
+    return;
+  }
+
+  if (dateStart > dateEnd) {
+    console.error("Error: your start date is later than end");
+    return;
+  }
+  return passedDays;
+}
+
+console.log(getDiffDays("2020-01-01", "2020-01-17")); // 16
+console.log(getDiffDays("2020-01-01", "2020-03-15")); // 74
+console.log(getDiffDays("2222222222", "2020-03-15")); // Error: invalid date
+console.log(getDiffDays("2021-01-02", "2020-03-15")); // Error: your start date is later than end
