cbd22affffacaa0f2ebc79ca63f996e4dee30c52
Victoria <victoriyasotscaya2050@gmail.com>
2022-02-24 00:57:03 +0200

    Added Task3

--- script.js
+++ script.js
@@ -36,3 +36,19 @@ console.log(getDiffDays("2020-01-01", "2020-01-17")); // 16
 console.log(getDiffDays("2020-01-01", "2020-03-15")); // 74
 console.log(getDiffDays("2222222222", "2020-03-15")); // Error: invalid date
 console.log(getDiffDays("2021-01-02", "2020-03-15")); // Error: your start date is later than end
+
+//Task3
+function isWeekend(date) {
+  const newDate = new Date(date);
+  const day = newDate.getDay();
+
+  if (day === 0 || day === 6) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isWeekend("2022-02-12")); // true
+console.log(isWeekend("2022-02-13")); // true
+console.log(isWeekend("2022-02-09")); // false
